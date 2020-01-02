ac178de0c62065f7120c946cf0b3460b826c7331
Swair Shah <swairshah@gmail.com>
2020-01-01 20:22:13 -0800

    dqn first steps

---
+++ q-learning/main.py
@@ -0,0 +1,41 @@
+from agent import Agent
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+
+
+env = gym.make('FrozenLake-v0')
+n_actions = env.action_space.n
+n_states = env.observation_space.n
+
+A = Agent(lr=0.001, gamma=0.9, 
+          eps_max = 1.0, eps_min = 0.01, eps_dec = 0.9999995, 
+          n_actions = n_actions, n_states = n_states)
+
+n_episodes = 5000000
+win_pct_list = []
+scores = []
+
+for i in range(n_episodes):
+    done = False
+    score = 0
+    s = env.reset()
+    done = False
+
+    while not done:
+        a = A.pick_action(s)
+        s_, r, done, info = env.step(a)
+        A.learn(s, a, r, s_)
+        score += r
+        s = s_
+        
+    scores.append(score)
+    if i % 100 == 0:
+        win_pct = np.mean(scores[-100:])
+        win_pct_list.append(win_pct)
+        if i % 1000 == 0:
+            print('episode', i, 'win pct %.2f' % win_pct, 
+                                'eps %2.f' % A.eps)
+
+plt.plot(win_pct_list)
+plt.show()
